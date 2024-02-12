1c87499c9b773f3d61104142ef37706c416a02ce
guo <b22790188@gmail.com>
2024-02-12 22:46:47 +0800

    connect to redis server and add user router

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,51 @@
 import express, { Request, Response } from 'express';
+import session from 'express-session';
 import { createClient } from 'redis';
+import RedisStore from 'connect-redis';
 import userRouter from './routes/userRouter';
+
+declare module 'express-session' {
+  export interface SessionData {
+    user: string;
+  }
+}
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 //todo: modify type
-export let client: any;
-
-const fakeData: { [key:string]: string} = {
-  admin: 'admin',
-  user: 'user',
-};
+export let redisClient = createClient();
 
 (async () => {
-  client = await createClient()
+  await redisClient
     .on('error', (err) => {
       console.log('Redis error' + err);
     })
     .connect();
+  console.log('connect redis success');
 })();
 
-export async function checkCache(req: Request, res: Response): Promise<boolean | undefined> {
-  const username = req.body.username;
-  let data = await client.get(username);
-  if (data) {
-    return data;
-  }
-}
+const redisStore = new RedisStore({
+  client: redisClient,
+});
 
+app.use(
+  session({
+    store: redisStore,
+    secret: 'secret',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      secure: false,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/login', userRouter)
+app.use('/login', userRouter);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello ts-express');
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,28 +1,41 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { client, checkCache } from '..';
+import { redisClient } from '..';
 const router = express.Router();
 
 const fakeData: { [key: string]: string } = {
   admin: 'admin',
   user: 'user',
-  
 };
+
+//todo: move function to service folder
+async function checkCache(
+  req: Request
+): Promise<string | undefined> {
+  const username = req.body.username;
+  let data = await redisClient.get(username);
+  if (data) {
+    return data;
+  }
+}
+
 
 router.post('/', async function (req: Request, res: Response, next: any) {
   const { username, password } = req.body;
-  let dataCached = await checkCache(req, res);
+  // console.log(req.session);
+  let dataCached = await checkCache(req);
   if (dataCached) {
     res.json({
       data: dataCached,
       info: 'data from cache',
     });
-    
+
     return;
   }
 
   if (username in fakeData && fakeData[username] === password) {
-    await client.set(username, password);
+    req.session.user = username;
+    await redisClient.set(username, password);
     res.send('Login success');
   } else {
     res.send('Login failed');
